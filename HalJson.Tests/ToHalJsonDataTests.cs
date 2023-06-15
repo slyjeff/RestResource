ee73448822d36eafa98b4b9562ab334a3558a249
Jeff Sylvester <sylvesterjj@gmail.com>
2023-06-15 01:32:09 -0400

    Added object, list, and dictonary types to json

--- HalJson.Tests/ToHalJsonDataTests.cs
+++ HalJson.Tests/ToHalJsonDataTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestResource;
 using RestResource.Extensions;
@@ -25,4 +26,57 @@ public class ToHalJsonDataTests {
         var expectedJson = $"{{{Environment.NewLine}  \"stringValue\": \"{stringValue}\",{Environment.NewLine}  \"intValue\": \"{intValue}\"{Environment.NewLine}}}";
         Assert.AreEqual(expectedJson, json);
     }
+
+    [TestMethod]
+    public void ObjectDataMustBeConvertedToJson() {
+        //arrange
+        var testObject = new TestObject();
+
+        var resource = new Resource()
+            .Data("testObject", testObject);
+
+        //act
+        var json = resource.ToHalJson();
+
+        //assert
+        var expectedJson = $"{{{Environment.NewLine}  \"testObject\": {{{Environment.NewLine}    \"stringValue\": \"{testObject.StringValue}\",{Environment.NewLine}    \"intValue\": \"{testObject.IntValue}\"{Environment.NewLine}  }}{Environment.NewLine}}}";
+        Assert.AreEqual(expectedJson, json);
+    }
+
+    [TestMethod]
+    public void ArrayDataValuesMustBeConvertedToJson() {
+        //arrange
+        var strings = new List<string> {
+            GenerateRandom.String(),
+            GenerateRandom.String(),
+            GenerateRandom.String()
+        };
+
+        var resource = new Resource()
+            .Data("strings", strings);
+
+        //act
+        var json = resource.ToHalJson();
+
+        //assert
+        var expectedJson = $"{{{Environment.NewLine}  \"strings\": [{Environment.NewLine}    \"{strings[0]}\",{Environment.NewLine}    \"{strings[1]}\",{Environment.NewLine}    \"{strings[2]}\"{Environment.NewLine}  ]{Environment.NewLine}}}";
+        Assert.AreEqual(expectedJson, json);
+    }
+
+    [TestMethod]
+    public void ArrayObjectsMustBeConvertedToJson() {
+        //arrange
+        var dataObjects = new List<TestObject> { new(), new(), new() };
+
+        var resource = new Resource()
+            .Data("dataObjects", dataObjects);
+
+        //act
+        var json = resource.ToHalJson();
+
+        //assert
+        var expectedJson = $"{{{Environment.NewLine}  \"dataObjects\": [{Environment.NewLine}    {{{Environment.NewLine}      \"stringValue\": \"{dataObjects[0].StringValue}\",{Environment.NewLine}      \"intValue\": \"{dataObjects[0].IntValue}\"{Environment.NewLine}    }},{Environment.NewLine}    {{{Environment.NewLine}      \"stringValue\": \"{dataObjects[1].StringValue}\",{Environment.NewLine}      \"intValue\": \"{dataObjects[1].IntValue}\"{Environment.NewLine}    }},{Environment.NewLine}    {{{Environment.NewLine}      \"stringValue\": \"{dataObjects[2].StringValue}\",{Environment.NewLine}      \"intValue\": \"{dataObjects[2].IntValue}\"{Environment.NewLine}    }}{Environment.NewLine}  ]{Environment.NewLine}}}";
+        Assert.AreEqual(expectedJson, json);
+    }
+
 }
---
+++ HalJson/ToHalJsonExtensions.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json.Linq;
+using RestResource;
+
+namespace HalJson; 
+
+public static class ToHalJsonExtensions {
+    public static string ToHalJson(this Resource resource) {
+        var o = new JObject();
+        foreach (var data in resource.Data) {
+            o.AddData(data);
+        }
+
+        if (!string.IsNullOrEmpty(resource.Uri)) {
+            o.AddLink(new Link("self", resource.Uri));
+        }
+
+        return o.ToString();
+    }
+
+    private static void AddData(this JObject o, KeyValuePair<string, object?> data) {
+        if (data.Value is string stringValue) {
+            o[data.Key] = stringValue;
+            return;
+        }
+
+        if (data.Value is IList<object?> listOfObjects) {
+            var array = new JArray();
+            foreach (var item in listOfObjects) {
+                array.Add(item);
+            }
+            o[data.Key] = array;
+        }
+
+        if (data.Value is IList<IDictionary<string, object?>> listOfDictionary) {
+            var array = new JArray();
+            foreach (var dictionary in listOfDictionary) {
+                array.Add(dictionary.ToJson());
+            }
+            o[data.Key] = array;
+        }
+
+        if (data.Value is IDictionary<string, object?> dictionaryObject) {
+            o[data.Key] = dictionaryObject.ToJson();
+        }
+    }
+
+    private static JObject ToJson(this IDictionary<string, object?> dictionary) {
+        var jObject = new JObject();
+        foreach (var item in dictionary) {
+            jObject.AddData(item);
+        }
+
+        return jObject;
+    }
+
+    private static void AddLink(this JObject o, ILink link) {
+        if (!o.ContainsKey("_links")) {
+            o["_links"] = new JObject();
+        }
+
+        var links = o["_links"];
+        if (links == null) {
+            return;
+        }
+
+        var linkObject = new JObject {
+            ["href"] = link.Uri
+        };
+
+        links[link.Name] = linkObject;
+    }
+}
