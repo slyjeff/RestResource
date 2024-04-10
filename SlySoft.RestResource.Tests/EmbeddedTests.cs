73c5c229bbfc83045d284b52acd68bdb077e7da6
Jeff Sylvester <sylvesterjj@gmail.com>
2024-04-10 06:11:45 -0400

    removes old hal nand adds back tests

---
+++ SlySoft.RestResource.Tests/EmbeddedTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestUtils;
+
+namespace SlySoft.RestResource.Tests;
+
+[TestClass]
+public sealed class EmbeddedTests {
+    [TestMethod]
+    public void MustBeAbleToAddAnEmbeddedResource() {
+        //arrange
+        var message = GenerateRandom.String();
+        var embeddedResource = new Resource()
+            .Data("message", message);
+
+        //act
+        var resource = new Resource()
+            .Embedded("subResource", embeddedResource);
+
+        //assert
+        var subResource = resource.GetEmbedded("subResource");
+        Assert.IsNotNull(subResource);
+        Assert.AreEqual(message, subResource.Data["message"]);
+    }
+
+    [TestMethod]
+    public void MustBeAbleToAddAListOfEmbeddedResources() {
+        //arrange
+        var message1 = GenerateRandom.String();
+        var message2 = GenerateRandom.String();
+
+        var embeddedResource1 = new Resource()
+            .Data("message", message1);
+        
+        var embeddedResource2 = new Resource()
+            .Data("message", message2);
+
+        //act
+        var resource = new Resource()
+            .Embedded("subResources", new List<Resource> { embeddedResource1, embeddedResource2 });
+
+        //assert
+        var subResources = resource.GetEmbeddedList("subResources");
+        Assert.IsNotNull(subResources);
+        Assert.AreEqual(message1, subResources[0].Data["message"]);
+        Assert.AreEqual(message2, subResources[1].Data["message"]);
+    }
+}
---
+++ SlySoft.RestResource.Tests/UriTests.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestUtils;
+
+namespace SlySoft.RestResource.Tests;
+
+[TestClass]
+public sealed class UriTests {
+    [TestMethod]
+    public void MustBeABleToSpecifyResourceUri() {
+        //arrange
+        var resource = new Resource();
+        var uri = GenerateRandom.String();
+
+        //act
+        resource.Uri(uri);
+
+        //assert
+        Assert.AreEqual(uri, resource.Uri);
+    }
+
+    [TestMethod]
+    public void UriMustBeChainable() {
+        //arrange
+        var uri = GenerateRandom.String();
+        var message = GenerateRandom.String();
+        
+        //act
+        var resource = new Resource()
+            .Uri(uri)
+            .Data("message", message);
+
+        //assert
+        Assert.AreEqual(uri, resource.Uri);
+        Assert.AreEqual(message, resource.Data["message"]);
+    }
+}
